571851560c082341d999d1e029c645bec448c22b
Himan Yerrakalva <yerrak@umich.edu>
2020-10-10 20:08:49 -0400

    Himan's initial commit

--- src/qr_scan/scripts/qr_scanner.py
+++ src/qr_scan/scripts/qr_scanner.py
@@ -8,3 +8,38 @@
 # run these two commands to launch fake display program:
 # source devel/setup.bash
 # rosrun url_display fake_display.py
+
+import cv2
+import numpy as np
+import sys
+import requests
+import time
+from pathlib import Path
+
+print(Path('.').absolute())
+r = requests.get("https://live.staticflickr.com/3048/2854521482_5bd41a2e1b_b.jpg").content
+#r = requests.get(sys.argv[1]).content
+nparr = np.frombuffer(r, np.uint8)
+inputImage = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+# inputImage = cv2.imread(sys.argv[1])
+
+def display(im, bbox):
+    n = len(bbox)
+    for j in range(n):
+        cv2.line(im, tuple(bbox[j][0]), tuple(bbox[ (j+1) % n][0]), (255, 0, 0), 3)
+    cv2.imshow("Results", im)
+
+qrDecoder = cv2.QRCodeDetector()
+
+data, bbox, rectifiedImage = qrDecoder.detectAndDecode(inputImage)
+if len(data)>0:
+    print("Decoded Data : {}".format(data))
+    display(inputImage, bbox)
+    rectifiedImage = np.uint8(rectifiedImage)
+    cv2.imshow("Rectified QRCode", rectifiedImage)
+else:
+    print("QR Code not detected")
+    cv2.imshow("Results", inputImage)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- src/url_display/scripts/oled_display.py
+++ src/url_display/scripts/oled_display.py
@@ -8,3 +8,48 @@
 # run these two commands to launch random url publisher:
 # source devel/setup.bash
 # rosrun qr_scan random_url.py
+
+import rospy
+from std_msgs.msg import String
+
+from board import SCL, SDA
+import busio
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_ssd1306
+
+class Display:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        i2c = busio.I2C(SCL, SDA)
+        self.disp = adafruit_ssd1306.SSD1306_I2C(width, height, i2c)
+        self.disp.fill(0)
+        self.disp.show()
+        self.image = Image.new("1", (width, height))
+        self.draw = ImageDraw.Draw(self.image)
+        self.font = ImageFont.truetype('/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf')
+
+    def clear(self):
+        self.disp.fill(0)
+        self.disp.show()
+
+    def show_text(self, text):
+        self.draw.rectangle((0, 0, self.width, self.height), outline=0, fill=0)
+        self.draw.text((0, 0), text, font=self.font, fill=255)
+        self.disp.image(self.image)
+        self.disp.show()
+
+def callback(data, disp):
+    print("Received URL:", data.data)
+    disp.show_text(data.data)
+
+def listener():
+    rospy.init_node("receiver", anonymous=True)
+    oled_display = Display(128, 64)
+    rospy.Subscriber("url", String, callback, oled_display)
+    print("Showing URLs on Display...")
+    rospy.spin()
+    oled_display.clear()
+
+if __name__ == "__main__":
+    listener()
