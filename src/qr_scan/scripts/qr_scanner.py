56c93b3f5e6f9f95e58de4c0008b1ae0d4383cae
Himan Yerrakalva <yerrak@umich.edu>
2020-10-11 17:22:52 -0400

    deleted all qr code and added print statement

--- src/qr_scan/scripts/qr_scanner.py
+++ src/qr_scan/scripts/qr_scanner.py
@@ -16,30 +16,32 @@ import requests
 import time
 from pathlib import Path
 
-print(Path('.').absolute())
-r = requests.get("https://live.staticflickr.com/3048/2854521482_5bd41a2e1b_b.jpg").content
-#r = requests.get(sys.argv[1]).content
-nparr = np.frombuffer(r, np.uint8)
-inputImage = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
-# inputImage = cv2.imread(sys.argv[1])
+print("hello world this is the qr scanner")
 
-def display(im, bbox):
-    n = len(bbox)
-    for j in range(n):
-        cv2.line(im, tuple(bbox[j][0]), tuple(bbox[ (j+1) % n][0]), (255, 0, 0), 3)
-    cv2.imshow("Results", im)
+# print(Path('.').absolute())
+# r = requests.get("https://live.staticflickr.com/3048/2854521482_5bd41a2e1b_b.jpg").content
+# #r = requests.get(sys.argv[1]).content
+# nparr = np.frombuffer(r, np.uint8)
+# inputImage = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+# # inputImage = cv2.imread(sys.argv[1])
 
-qrDecoder = cv2.QRCodeDetector()
+# def display(im, bbox):
+#     n = len(bbox)
+#     for j in range(n):
+#         cv2.line(im, tuple(bbox[j][0]), tuple(bbox[ (j+1) % n][0]), (255, 0, 0), 3)
+#     cv2.imshow("Results", im)
 
-data, bbox, rectifiedImage = qrDecoder.detectAndDecode(inputImage)
-if len(data)>0:
-    print("Decoded Data : {}".format(data))
-    display(inputImage, bbox)
-    rectifiedImage = np.uint8(rectifiedImage)
-    cv2.imshow("Rectified QRCode", rectifiedImage)
-else:
-    print("QR Code not detected")
-    cv2.imshow("Results", inputImage)
+# qrDecoder = cv2.QRCodeDetector()
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# data, bbox, rectifiedImage = qrDecoder.detectAndDecode(inputImage)
+# if len(data)>0:
+#     print("Decoded Data : {}".format(data))
+#     display(inputImage, bbox)
+#     rectifiedImage = np.uint8(rectifiedImage)
+#     cv2.imshow("Rectified QRCode", rectifiedImage)
+# else:
+#     print("QR Code not detected")
+#     cv2.imshow("Results", inputImage)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
