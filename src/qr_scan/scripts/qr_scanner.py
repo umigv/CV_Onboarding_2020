2d3bc2018eceb24ead5ddfb77b311ca96d2d0179
ashmoney024 <71104759+ashmoney024@users.noreply.github.com>
2020-10-25 17:52:11 -0400

    finished part 1 qr scan

--- src/qr_scan/scripts/qr_scanner.py
+++ src/qr_scan/scripts/qr_scanner.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# Write your QR scanner code here!
-
 # run these two commands to launch your qr scanner code:
 # source devel/setup.bash
 # rosrun qr_scan qr_scanner.py
@@ -11,8 +8,12 @@
 
 import cv2
 import time
-from picamera.array import PiRBGArray
+import numpy as np
+import sys
+from picamera.array import PiRGBArray
 from picamera import PiCamera
+import rospy
+from std_msgs.msg import String
 
 
 # initialize camera and grab reference to raw camera capture
@@ -22,25 +23,44 @@ rawCapture = PiRGBArray(camera)
 # allow camera to sleep for 0.1 sec
 time.sleep(0.1)
 
-# get image from camera
-camera.Capture(rawCapture, format ="bgr")
-image = rawCapture.array
-
-#display image on screen and wait for key press
-cv2.imshow("Image", image)
-cv2.waitKey(0)
 
 
-# CONVERTING QR CODE TO STRING
 
-if len(sys.argv) > 1:
-	inputImage = cv2.imread(sys.argv[1])
-else:
-	inputImage = cv2.imread("qr_codes/screen-0.jpg")
-  
-  
-decodedObjects = pyzbar.decode(inputImage)
+# WRITING THE PUBLISHER NODE
 
-print(decodedObjects[0].data)
+def talker():
+	pub = rospy.Publisher('url', String, queue_size=10)
+	rospy.init_node('talker', anonymous=True)
+	rate = rospy.Rate(10) # 10hz
+	while not rospy.is_shutdown():
+  	# get image from camera
+		camera.Capture(rawCapture, format ="bgr")
+		image = rawCapture.array
+
+		#display image on screen and wait for key press
+		#cv2.imshow("Image", image)
+		#cv2.waitKey(0)
+
+		#if len(sys.argv) > 1:
+		#	inputImage = cv2.imread(sys.argv[1])
+		#else:
+		#	inputImage = cv2.imread("qr_codes/screen-0.jpg")
+      
+		decodedObjects = pyzbar.decode(image)
+		print(decodedObjects[0].data)
+    hello_str = decodedObjects.data
+    rospy.loginfo(hello_str)
+    pub.publish(hello_str)
+    rate.sleep()
+    
+
+
+
+if __name__ == '__main__':
+  try:
+  	talker()
+  except rospy.ROSInterruptException:
+  	pass
+
 
 
